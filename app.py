16b29abca642f50aee5b09399d19b13f0a5ba6cc
atharvaawatade <97244560+atharvaawatade@users.noreply.github.com>
2024-08-31 11:27:28 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def main():
+    st.title("Website Redirection")
+
+    st.write("Click the links below to redirect to respective websites:")
+
+    st.markdown("[User Page](https://hospital-management-user.streamlit.app/)")
+    st.markdown("https://finagent-stockpredection-ldlkksk2ytucktwfswlwxy.streamlit.app)")
+
+if name == "main":
+    main()
